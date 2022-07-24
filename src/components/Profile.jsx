20a9b553bbc8fa93f3d66464ceb4be3f6ad7cb03
Amironac <61164084+Amironac@users.noreply.github.com>
2022-07-24 13:33:13 +0200

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import {toast} from 'react-toastify'
 import { useState,useEffect } from 'react'
-import {useNavigate} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import { FaArrowLeft, FaFacebook, FaInstagram, FaLinkedin, FaShoppingCart, FaTwitter } from 'react-icons/fa'
 
 function Profile() {
   const user = JSON.parse(localStorage.getItem('user'))
@@ -13,8 +14,25 @@ function Profile() {
     } 
   },[])
   return (
-    <div>
-      
+    <div className='profile-container'>
+      <div className='profile-background'>
+        <img src="https://cdn-icons-png.flaticon.com/512/306/306473.png" alt="" />
+      </div>
+
+      <div className='profile-text'> 
+        <h1 className='secondary-color'> Dobrodošli {user.name} </h1>
+        <ul>
+          <li><FaFacebook /></li>
+          <li><FaInstagram /></li>
+          <li><FaTwitter/></li>
+          <li><FaLinkedin /></li>
+        </ul>
+
+        <ul>
+          <li><h3><Link to='/'>Idi na kupovinu</Link></h3></li>
+          <li><h3><Link to='/cart'><FaShoppingCart /> Vaša košarica </Link></h3></li>
+        </ul>
+      </div>
     </div>
   )
 }
